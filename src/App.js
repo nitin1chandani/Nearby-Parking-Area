c8af78427c09217a68b64429322b292ba4c98c02
Nitin Chandani <nitin1chandani@gmail.com>
2023-04-09 23:59:22 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import Home from "./screens/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+
 function App() {
   return (
-    <div className="">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <h1>body</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
