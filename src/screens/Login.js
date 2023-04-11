8df95acc3729e7cde5d60529836a848b9fc33150
Nitin Chandani <nitin1chandani@gmail.com>
2023-04-11 23:13:06 +0530

    forms

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,32 +1,60 @@
 import React, { useState } from "react";
 
-const Login = () => {
+function Login() {
   const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const submitHandler = (event) => {
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(`Submitting ${email} and ${password}`);
   };
+
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="email">email</label>
-      <input
-        value={email}
-        type="email"
-        placeholder="youremail@gmail.com"
-        id="email"
-        name="email"
-      />
-      <label htmlFor="password">password</label>
-      <input
-        value={pass}
-        type="password"
-        placeholder="********"
-        id="password"
-        name="password"
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+            email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="abc@gmail.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
 export default Login;
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,11 +1,104 @@
-import React from "react";
+import { useState } from "react";
 
-const Register = () => {
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(
+      `Email: ${email}, Password: ${password}, Confirm Password: ${confirmPassword}`
+    );
+  };
+
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="min-h-screen  flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
+          Register
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="bg-gray-100 text-gray-900 rounded-lg px-4 py-2 w-full"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="bg-gray-100 text-gray-900 rounded-lg px-4 py-2 w-full"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirm password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="confirm-password"
+                  name="confirm-password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className="bg-gray-100 text-gray-900 rounded-lg px-4 py-2 w-full"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Register;
